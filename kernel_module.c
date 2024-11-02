9283fcde8c950b361f7c85bd012e5d0f0a8f2c92
sequint <steve.e.quint@gmail.com>
2024-11-02 19:35:39 +0000

    Issue #7: Create temp thresholds and monitorProcess func skeleton

--- kernel_module.c
+++ kernel_module.c
@@ -1,5 +1,14 @@
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/sched.h>
+#include <linux/init.h>
+#include <linux/seq_file.h>
+
+// Temporary threshold definitions
+#define CPU_THRESHOLD 80
+#define MEM_THRESHOLD (100 * 1024) // 100MB
+
+void monitorProcesses(void);
 
 int init_module(void)
 {
@@ -12,7 +21,12 @@ void cleanup_module(void)
     pr_info("Kernel Module Unloaded\n");
 }
 
+void monitorProcesses(void)
+{
+    struct task_stuct *task;
+}
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Steven Quintana, Mason Wilson IV");
 MODULE_DESCRIPTION("Anomoly Detection Kernel Module");
-MODULE_VERSION("1.0");
+MODULE_VERSION("1.1");
