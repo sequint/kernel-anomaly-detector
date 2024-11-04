b44a4d3888763b4165436a26f4462f81260dac14
sequint <steve.e.quint@gmail.com>
2024-11-04 11:24:36 -0800

    Closes #7: Created monitor function skeleton - Function uses task_struct to monitor and flag cpu & mem usage anomolies - Temp static thresholds set - Updated load and unload

--- kernel_module.c
+++ kernel_module.c
@@ -3,28 +3,52 @@
 #include <linux/sched.h>
 #include <linux/init.h>
 #include <linux/seq_file.h>
+#include <linux/mm.h>
 
 // Temporary threshold definitions
 #define CPU_THRESHOLD 80
 #define MEM_THRESHOLD (100 * 1024) // 100MB
 
-void monitorProcesses(void);
-
-int init_module(void)
+static void monitorProcesses(void)
 {
+    // Set a task struct pointer to use for each process
+    struct task_struct *task;
+    bool anomaly_found = false;
+
+    for_each_process(task)
+    {
+        unsigned long cpu_usage = task->utime + task->stime;  // CPU usage based on sum of user and system time for a process
+        unsigned long mem_usage = (task->mm) ? get_mm_rss(task->mm) * PAGE_SIZE / 1024 : 0;  // Memory usage for the process converted from KB to MB
+
+        // If either exceed predefined thresholds, log the process
+        if (cpu_usage > CPU_THRESHOLD || mem_usage > MEM_THRESHOLD)
+        {
+            printk(KERN_INFO "Flagged Anomoly Process: %s, PID: %d, CPU: %lu, Memory: %lu MB\n", task->comm, task->pid, cpu_usage, mem_usage);
+            anomaly_found = true;
+        }
+    }
+
+    if (!anomaly_found)
+    {
+        printk(KERN_INFO "No anomoly processes found\n");
+    }
+}
+
+static int __init anomaly_module_init(void)
+{
+
     pr_info("Kernel Module Loaded\n");
+    monitorProcesses();
     return 0;
 }
 
-void cleanup_module(void)
+static void __exit anomaly_module_exit(void)
 {
     pr_info("Kernel Module Unloaded\n");
 }
 
-void monitorProcesses(void)
-{
-    struct task_stuct *task;
-}
+module_init(anomaly_module_init);
+module_exit(anomaly_module_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Steven Quintana, Mason Wilson IV");
