2d7d8a5f2e419bde412df5525a6f273be07c09b7
sequint <steve.e.quint@gmail.com>
2024-11-20 12:53:00 -0800

    Issue #13: Consolidated Process Logs - For each process, if anomaly found, prints one title with process information - Under title, only thresholds reached are logged for each task

--- kernel_module.c
+++ kernel_module.c
@@ -5,14 +5,13 @@
 #include <linux/seq_file.h>
 #include <linux/mm.h>
 
-// Temporary threshold definitions
+// Static threshold definitions
 #define CPU_THRESHOLD 80
 #define MEM_THRESHOLD (100 * 1024) // 100MB
 
 static void monitorProcesses(void)
 {
-    // Set a task struct pointer to use for each process
-    struct task_struct *task;
+    struct task_struct *task; // Set a task struct pointer to use for each process
     bool anomaly_found = false;
 
     for_each_process(task)
@@ -20,17 +19,27 @@ static void monitorProcesses(void)
         unsigned long cpu_usage = task->utime + task->stime;  // CPU usage based on sum of user and system time for a process
         unsigned long mem_usage = (task->mm) ? get_mm_rss(task->mm) * PAGE_SIZE / 1024 : 0;  // Memory usage for the process converted from KB to MB
 
-        // If either exceed predefined thresholds, log the process
         if (cpu_usage > CPU_THRESHOLD || mem_usage > MEM_THRESHOLD)
         {
-            printk(KERN_INFO "Flagged Anomoly Process: %s, PID: %d, CPU: %lu, Memory: %lu MB\n", task->comm, task->pid, cpu_usage, mem_usage);
+            printk(KERN_INFO "\nFlagged Anomoly for Process: %s, PID: %d\n", task->comm, task->pid);
+
+            // Log all anomalies found for this process
+            if (cpu_usage > CPU_THRESHOLD)
+            {
+                printk(KERN_INFO "CPU Usage: %lu\n", cpu_usage);
+            }
+            if (mem_usage > MEM_THRESHOLD)
+            {
+                printk(KERN_INFO "Memory Usage: %lu\n", mem_usage);
+            }
+
             anomaly_found = true;
         }
     }
 
     if (!anomaly_found)
     {
-        printk(KERN_INFO "No anomoly processes found\n");
+        printk(KERN_INFO "No process anomalies found\n");
     }
 }
 
